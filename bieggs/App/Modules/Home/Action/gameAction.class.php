41d61bf69b15e6d9e15b353e2e9523a2d3fc8132
DingYong4223 <dingyong4223@163.com>
2014-06-04 09:23:04 +0800

    add file

---
+++ bieggs/App/Modules/Home/Action/gameAction.class.php
@@ -0,0 +1,14 @@
+<?php
+class gameAction extends Action{
+    public function index(){
+        $this->display();
+    }
+
+    public function pg310(){
+        $this->display();
+    }
+
+    public function pgstar(){
+        $this->display();
+    }
+}
---
+++ bieggs/App/Modules/Home/Action/prizeAction.class.php
@@ -0,0 +1,6 @@
+<?php
+class prizeAction extends Action{
+    public function index(){
+        $this->display();
+    }
+}
