339edba1834629b5f4a750bb41a703a50f69dd5e
DingYong4223 <dingyong4223@163.com>
2014-07-15 18:02:41 +0800

    add all

---
+++ bieggs/App/Modules/Home/Action/msgAction.class.php
@@ -0,0 +1,8 @@
+<?php
+
+class msgAction extends Action{
+
+    public function index(){
+        $this->display();
+    }
+}
--- bieggs/App/Modules/Home/Action/userAction.class.php
+++ bieggs/App/Modules/Home/Action/userAction.class.php
@@ -1,9 +1,34 @@
 <?php
 
 class userAction extends Action{
+	function _initialize() {
+        $this->assign('PAGE_CURID',7);
+    }
+
     public function index(){
-    	$this->assign('PAGE_CURID',7);
+    	$this->assign('PAGE_NAVII',0); //基本信息
         $this->display();
     }
 
+    public function account(){
+    	$this->assign('PAGE_NAVII',1); //基本信息
+    	$sub_tab = ("" == $this->_param("sub_tab"))? 0 : $this->_param("sub_tab");
+    	$this->assign('SUB_TAB',$sub_tab); //重属tab
+    	
+        $this->display();
+    }
+
+    public function myprize(){
+        $this->assign('PAGE_NAVII',2); //我的兑奖
+        
+        $this->display();
+    }
+
+    public function edit(){
+        $this->assign('PAGE_NAVII',3); //修改资料
+        $sub_tab = ("" == $this->_param("sub_tab"))? 0 : $this->_param("sub_tab");
+        $this->assign('SUB_TAB',$sub_tab); //重属tab
+        
+        $this->display();
+    }
 }
