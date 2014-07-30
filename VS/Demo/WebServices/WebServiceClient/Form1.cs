08d7aa34d6142ec08e70b6f05f63281dc169d797
DingYong4223 <dingyong4223@163.com>
2014-07-30 18:09:36 +0800

    add web services

---
+++ VS/Demo/WebServices/WebServiceClient/Form1.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+
+using ch8_2.localhost;
+ 
+namespace ch8_2
+{
+	/// <summary>
+	/// Form1 的摘要说明。
+	/// </summary>
+	public class Form1 : System.Windows.Forms.Form
+	{
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.Container components = null;
+
+		public Form1()
+		{
+			//
+			// Windows 窗体设计器支持所必需的
+			//
+			InitializeComponent();
+
+			//
+			// TODO: 在 InitializeComponent 调用后添加任何构造函数代码
+			//
+		}
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if (components != null) 
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要使用代码编辑器修改
+		/// 此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(192, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "显示时间";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(32, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "使用Web Service的方法显示时间";
+            // 
+            // Form1
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(6, 14);
+            this.ClientSize = new System.Drawing.Size(292, 221);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "使用XML Web Service";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+		#endregion
+
+		/// <summary>
+		/// 应用程序的主入口点。
+		/// </summary>
+		[STAThread]
+		static void Main() 
+		{
+			Application.Run(new Form1());
+		}
+
+		private void button1_Click(object sender, System.EventArgs e)
+		{
+			try
+			{ 
+				//创建一个Web Service的实例
+                Service service = new Service();
+				//调用其中的方法
+                this.textBox1.Text = service.HelloWorld2();
+			}
+			catch (Exception error)
+			{
+			   //错误处理
+			   MessageBox.Show("调用Web Service时出现错误："+error.Message,"错误",
+				   MessageBoxButtons.OK,MessageBoxIcon.Warning);
+			}
+		}
+	}
+}
---
+++ VS/Demo/WebServices/WebService_3.5/App_Code/Service.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using MySQLDriverCS;
+
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消注释以下行。
+// [System.Web.Script.Services.ScriptService]
+
+public class Service : System.Web.Services.WebService
+{
+    public Service () {
+
+        //如果使用设计的组件，请取消注释以下行
+        //InitializeComponent(); 
+    }
+
+    [WebMethod(Description="this is my first test")]
+    public string HelloWorld() {
+        Console.Write("this s s s s s");
+        return "this is a test";
+    }
+
+    [WebMethod(Description = "insert data to my db")]
+    public string HelloWorld2()
+    {
+        MySQLConnection conn = null;
+        try
+        {
+            string connstr = "Data Source=bieggs;Password=;User ID=root;Location=localhost";
+            conn = new MySQLConnection(connstr);
+            conn.Open();
+            string query = "insert into bi_tiyan values(14, 'testname', '2000', 'test.jpg','1')";
+            MySQLCommand cmd = new MySQLCommand(query, conn);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            conn.Close();
+        }
+        return "this is a test2";
+    }
+    
+}
